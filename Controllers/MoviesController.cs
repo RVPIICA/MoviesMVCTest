09b16849dffbb864c8696e748a9cc434e90132e1
Randall Vargas <randall.vargas@iica.int>
2020-03-25 15:24:28 -0600

    Agrego la posibildad de buscar por título.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,9 +20,17 @@ namespace Movies.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string search)
         {
-            return View(await _context.Movie.ToListAsync());
+            var movies = from m in _context.Movie
+                         select m;
+
+            if(!string.IsNullOrEmpty(search))
+            {
+                movies = movies.Where(s => s.Title.Contains(search));
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         // GET: Movies/Details/5
